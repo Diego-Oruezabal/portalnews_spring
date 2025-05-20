f8178777f02643e84367d8461fdcecef7e9486e9
Diego Oruezabal <diegooruezabal@gmail.com>
2025-05-20 20:36:12 +0200

    Configuración database

---
+++ src/main/java/com/portal/backend/portalNews/PortalNewsApplication.java
@@ -0,0 +1,13 @@
+package com.portal.backend.portalNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portal/backend/portalNews/PortalNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.backend.portalNews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
